6dc88b204efa46ec837247311a68f400a187bc39
Lorenzo Squarcia <lsquarci@e3r6p7.42roma.it>
2023-11-14 14:31:36 +0100

    norm

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,12 +6,12 @@
 /*   By: lsquarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:54:53 by lsquarci          #+#    #+#             */
-/*   Updated: 2023/11/14 13:54:59 by lsquarci         ###   ########.fr       */
+/*   Updated: 2023/11/14 14:30:55 by lsquarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
