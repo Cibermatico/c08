c391d5f5f22965d8c2beb88540b936dc0b7210c6
Lorenzo Squarcia <lsquarci@e3r11p7.42roma.it>
2023-11-15 17:14:54 +0100

    sistemato il ex01, manca la norma

---
+++ ex01/ft_boolean.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsquarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:34:43 by lsquarci          #+#    #+#             */
+/*   Updated: 2023/11/14 15:35:42 by lsquarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_boolean.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+t_bool	ft_is_even(int nbr)
+{
+	if (EVEN(nbr))
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
